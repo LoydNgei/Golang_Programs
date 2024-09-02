18b67e76f6e4aad4e2633aa41dd9b34e9f63c76a
LoydNgei <loydngei@gmail.com>
2024-09-02 15:37:03 +0300

    Variables lesson

---
+++ variables.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	// "reflect"
+)
+
+func main() {
+
+// Basic variable declaration
+
+	// var name string = "Loyd Mayweather"
+	// var age int = 22
+
+	// fmt.Printf("My name is %v and I am %v years old", name, age)
+
+
+// Check the type of using reflect package
+	// var x = "John"
+	// var y = 1999
+
+	// fmt.Println(reflect.TypeOf(x))
+	// fmt.Println(reflect.TypeOf(y))
+
+
+// Short variable declaration
+	// z := "Loyd"
+	// w := 45
+
+	// fmt.Println(z)
+	// fmt.Println(w)
+
+// Multiple variable declaration
+	// var fname, lname = "Loyd", "Dev"
+	// m, n, o := 5, 67, 56
+	// item, price := "Mobile", 1500
+
+
+	// fmt.Println(fname + lname)
+	// fmt.Println(item, "-", price)
+	// fmt.Println(m + n + o)
+}
