38a9c80e165171f4d8dea2f72e5bf779d376d10e
LoydNgei <loydngei@gmail.com>
2024-09-02 19:58:33 +0300

    constants in Go

---
+++ constants.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const (
+	name = "Loyd"
+	occupation = "Software test engineer"
+	organization = "Transsion holdings"
+)
+
+func main() {
+	fmt.Println(name)
+	fmt.Println(occupation)
+	fmt.Println(organization)
+}
