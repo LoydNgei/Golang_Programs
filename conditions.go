07799f2c300462f028c87dbb02aa467480c8c888
LoydNgei <loydngei@gmail.com>
2024-09-04 23:46:30 +0300

    Conditions in golang

--- conditions.go
+++ conditions.go
@@ -6,13 +6,16 @@ import (
 
 func main() {
 	x := 100
-
 	if x == 50 {
 		fmt.Println("Germany")
 	} else if x == 100 {
 		fmt.Println("Japan")
 	} else {
 		fmt.Println("Canada")
+	}
+
+	if x := 100; x == 100 {
+		fmt.Println("Germany")
 	}
 }
 
